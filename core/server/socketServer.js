28576f1981acb1f9ab29baa48e9cf96ad6863b04
Ashley Nolan <anolan@tmw.co.uk>
2014-11-24 20:16:28 +0000

    Updating server connection

--- core/server/socketServer.js
+++ core/server/socketServer.js
@@ -8,7 +8,8 @@ var io = require('socket.io'), //socket.io - used for our websocket connection;
 
 var SocketServer = {
 
-	client : clientio.connect('http://localhost:3001'),
+	//client : clientio.connect('http://localhost:3001'),
+	client : clientio.connect('http://christmas-tracker-daemon.herokuapp.com'),
 
 	init : function (app, server) {
 
